7669c6ab777ccbbba6867767cfc69bb97ca6781d
Anton Shablyklo <zx0fox@gmail.com>
2018-11-04 16:39:01 -0500

    Update project_4-text-message.py

--- project_4-text-message.py
+++ project_4-text-message.py
@@ -2,8 +2,8 @@
 
 from twilio.rest import TwilioRestClient
 
-account_sid = "ACfec25322253c43ce53fe13e917df9d08"
-auth_token = "888e106105837d590b43f23d43a671fb"
+account_sid = "******"
+auth_token = "******"
 client = TwilioRestClient(account_sid,auth_token)
 
 client.messages.create(
